a10ca47a8e1964ec6e51fccf1a509cdf5cfd2bae
zizogi <metaziz@gmail.com>
2024-03-09 19:48:15 +0100

    ajout6 source At tool

---
+++ src/main/java/com/dsv/ATTool/RunningSS.java
@@ -0,0 +1,43 @@
+package com.dsv.ATTool;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class RunningSS {
+ public static void main(String[] args) {
+  Process p;
+  try {
+   String[] cmd = { "ksh", "chmod 777 ./TestScript_KSH_v1OK.ksh;./TestScript_KSH_v1OK.ksh tester1"};
+   p = Runtime.getRuntime().exec(cmd); 
+   p.waitFor(); 
+   BufferedReader reader=new BufferedReader(new InputStreamReader(
+    p.getInputStream())); 
+   String line; 
+   while((line = reader.readLine()) != null) { 
+    System.out.println(line);
+   } 
+  } catch (IOException e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  } catch (InterruptedException e) {
+   // TODO Auto-generated catch block
+   e.printStackTrace();
+  }
+ }
+
+/*
+        if (args.length < 1) {
+            System.err.println("Please provide an input!");
+            System.exit(0);
+        }
+        System.out.println(sha256hex(args[0]));
+
+    }
+*/
+
+    public static String sha256hex(String input) {
+        return DigestUtils.sha256Hex(input);
+    }
+}
